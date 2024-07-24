04c2cac22b4152b3df9abd85784062608df60197
Larissa Ferreira <164913651+Larissarff@users.noreply.github.com>
2024-07-24 13:21:02 -0300

    Create EdicaoDeTabelas.sql

---
+++ Comandos do CRUD/EdicaoDeTabelas.sql
@@ -0,0 +1,8 @@
+-- C - create
+-- R - read
+-- U - update
+-- D - delete
+
+-- EXERCÍCIO: 
+-- CRIAR UMA DATABASE CHAMADA cadastro, CRIAR UMA TABELA CHAMADA pessoas COM COLUNAS: nome, cpf, rg e limite.
+-- 
