eded66e9f7a5590b174ab688c2a0a88e6d21fa4c
Larissa Ferreira <164913651+Larissarff@users.noreply.github.com>
2024-07-31 11:41:06 -0300

    Atualizar o Functions.sql

--- Avançando em Select /Functions.sql
+++ Avançando em Select /Functions.sql
@@ -11,3 +11,22 @@ SELECT MIN(salary) FROM salaries;
 -- Ambas retornam o maior salário:
 SELECT salary FROM salaries ORDER BY salary ASC LIMIT 1;
 SELECT MAX(salary) AS maior_salario FROM salaries;
+
+-- Traz o numero de resultados que se combinam com a querie e adciona em uma coluna "mulheres"
+SELECT COUNT(*) AS mulheres FROM employees WHERE gender = 'F';  -- results:120051
+
+-- FUNÇÃO AVG: traz a media daquele filtro
+SELECT AVG(salary) AS media FROM salaries WHERE salary > 100000;
+-- cria uma nova coluna "media" onde traz a media dos salários maiores que 100.000
+
+--FUNÇÃO SUM: retorna a soma dos valores do fltro
+SELEC SUM(salary) FROM salaries;  -- Soma de todos os valores da coluna salario
+
+SELECT SUM(salary) FROM salaries ORDER BY salary DESC LIMIT 5; -- # NAO FUNCIONA com ORDER BY LIMIT
+SELECT SUM(salary) FROM salaries WHERE salary > 100000;        -- Esse sim filtra correto
+
+-- Operador LIKE 
+SELECT * FROM employees WHERE first_name LIKE %ber%;
+-- selecione em todas as colunas da tabela employees todos os dado squando a coluna first_name tiver dados que contém "ber"
+-- ex: pessoas com nome bernardo, roberta, etc. terão todas as suas colunas exbidas.
+
