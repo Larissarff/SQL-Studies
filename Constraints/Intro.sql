4429344648ba64ee6cc859802d500aed0fa07a62
Larissa Ferreira <164913651+Larissarff@users.noreply.github.com>
2024-08-18 12:14:01 -0300

    Create Intro.sql

---
+++ Constraints/Intro.sql
@@ -0,0 +1,4 @@
+-- Regras que determinam como os campos serão preenchidos
+-- São adicionadas na criação das tabelas / Ajudam a organizar e padronizar as tabelas
+-- ex: 
+NOT NULL -- = o campo não pode ser nulo
